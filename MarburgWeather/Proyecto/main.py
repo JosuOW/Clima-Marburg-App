1d8a7c2bfbd250ca7d82e65b6bc3af8fe5469c87
Juan Murillo <juan.murillo@epn.edu.ec>
2025-02-08 19:05:29 -0500

    Agrege outputCrontab.log para registro de errores temporales

--- MarburgWeather/Proyecto/main.py
+++ MarburgWeather/Proyecto/main.py
@@ -6,7 +6,7 @@ def main():
     Punto de entrada del script que recibe argumentos para procesar los datos del clima.
     """
     parser = argparse.ArgumentParser(description="Procesa datos del clima desde output.log a CSV")
-    parser.add_argument("--log", type=str, default="/home/juanubuntu/ProyectonOseQpOner/Crontab/output.log", help="Ruta del archivo de log")
+    parser.add_argument("--log", type=str, default="/home/juanubuntu/Clima-Marburg-App/MarburgWeather/Proyecto/Crontab/output.log", help="Ruta del archivo de log")
     parser.add_argument("--csv", type=str, default="clima-Marburg-hoy.csv", help="Nombre del archivo CSV de salida")
     args = parser.parse_args()
     parse_weather_log(args.log, args.csv)
